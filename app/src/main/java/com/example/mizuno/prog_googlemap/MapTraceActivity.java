8c02ddf48900ba01201baf9e7812e81cb47886df
smzn <s.mzn.eng@gmail.com>
2016-01-25 15:50:26 +0900

    20160125 Submit

--- app/src/main/java/com/example/mizuno/prog_googlemap/MapTraceActivity.java
+++ app/src/main/java/com/example/mizuno/prog_googlemap/MapTraceActivity.java
@@ -1,15 +1,11 @@
 package com.example.mizuno.prog_googlemap;
 
 import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,21 +14,21 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,LocationListener {
+public class MapTraceActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_maptrace);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+        AsyncTaskGetJson taskGetJson = new AsyncTaskGetJson(this);
+        taskGetJson.execute();
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,6 +37,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mi.inflate(R.menu.option_menu, menu);
         return true;
     }
+
 
     /**
      * Manipulates the map once available.
@@ -62,88 +59,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 15));
     }
 
-    public void mapLocation(double latitude, double longitude){
+    public void mapLocation(double geo[]){
 
         mMap.clear();
-        LatLng latlng = new LatLng(latitude,longitude);
-        mMap.addMarker(new MarkerOptions().position(latlng));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 15));
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
-
-        TextView tv_lat = (TextView)findViewById(R.id.latitude);
-        tv_lat.setText("Latitude: " + latitude);
-
-        //経度の表示
-        TextView tv_lng = (TextView)findViewById(R.id.longitude);
-        tv_lng.setText("Longitude:" + longitude);
-
-        this.mapLocation(latitude, longitude);
-
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
-
-    @Override
-    protected void onPause() {
-
-        if(locationManager != null){
-            //位置情報の更新不要の場合は終了
-            locationManager.removeUpdates(this);
+        for(int i = 0; i < geo.length; i+=2) {
+            LatLng latlng = new LatLng(geo[i], geo[i+1]);
+            mMap.addMarker(new MarkerOptions().position(latlng));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 14));
         }
 
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-
-        if(locationManager != null){
-            //位置情報の更新を取得
-            locationManager.requestLocationUpdates(
-                    //permissionにACCESS_FINE_LOCATIONを追加
-                    LocationManager.GPS_PROVIDER,
-                    //NETWORK_PROVIDERを利用する場合はpermissionにACCESS_COARSE_LOCATIONを追加
-                    //LocationManager.NETWORK_PROVIDER
-                    //networkから取得する場合こちらに切り替える
-                    10000,// 通知のための最小時間間隔（ミリ秒）この場合は10秒に１回
-                    0,// 通知のための最小距離間隔（メートル）
-                    this
-            );
-        }
-
-        super.onResume();
     }
 
     public boolean onMenuItemSelected(int featureId, MenuItem item){
         super.onMenuItemSelected(featureId, item);
         switch(item.getItemId()){
             case R.id.item01:
-                Intent intent1 = new Intent(MapsActivity.this,
+                Intent intent1 = new Intent(MapTraceActivity.this,
                         MapsActivity.class); startActivity(intent1);
                 break;
             case R.id.item02:
-                Intent intent2 = new Intent(MapsActivity.this,
+                Intent intent2 = new Intent(MapTraceActivity.this,
                         MapTraceActivity.class); startActivity(intent2);
                 break;
         }
